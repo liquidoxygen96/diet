385755cfe6142e892c2887793294e1009db854d6
liquidoxygen96 <liquidoxygen96@protonmail.com>
2022-06-04 02:32:30 -0700

    helloWorld

---
+++ app.py
@@ -0,0 +1,7 @@
+# all together: use phsiology final results to tailor a specefic caloric and food intake information
+
+#Create a set of meal ideas that the person can make
+
+# Minimum Viable Product == check ----> FALSE (change to true when software works: Date: June 4th 2022 
+
+#Author 0xliquid
