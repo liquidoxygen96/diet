a6650501cdc0144485a2bf1ca16f6b0d30b24e90
liquidoxygen96 <liquidoxygen96@protonmail.com>
2022-06-04 02:25:50 -0700

    goal init

---
+++ api_retail.py
@@ -0,0 +1,7 @@
+# API CONNECTION TO RETAIL STORES: store necessary product data into a Python dictionary
+
+# Use the REST and JSON framework to get product metadata and store them in a queriable dictionary. 
+
+#Important customer variables: location
+
+#
